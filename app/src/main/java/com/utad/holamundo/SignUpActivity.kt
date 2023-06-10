c3638d5c4de9775f134a8c3d98290cf2eee9ab80
martamagui <73697174+martamagui@users.noreply.github.com>
2023-06-10 22:23:57 +0200

    Solución onclick

--- app/src/main/java/com/utad/holamundo/SignUpActivity.kt
+++ app/src/main/java/com/utad/holamundo/SignUpActivity.kt
@@ -1,11 +1,34 @@
 package com.utad.holamundo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.utad.holamundo.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignUp.setOnClickListener {
+            val username: String = binding.etUserName.text.toString()
+            val password: String = binding.etPassword.text.toString()
+            val description: String = binding.etUserDescription.toString()
+            navigateToProfile(username, password, description)
+        }
     }
+
+    private fun navigateToProfile(userNameValue: String, passwordValue: String, descriptionValue: String) {
+        val intent: Intent = Intent(this, ProfileActivity::class.java)
+        intent.putExtra("username", userNameValue)
+        intent.putExtra("password", passwordValue)
+        intent.putExtra("description", descriptionValue)
+
+        startActivity(intent)
+    }
+
+
 }
